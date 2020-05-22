71b48c21d6dded71fce307e1130275ae34b0a362
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2020-05-21 21:34:24 -0400

    Initial commit with idea

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+// Download the installer binary from mirror.openshift.com. URL to download
+// from:
+// http://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview/latest/
+// Following flags need to present
+// - platform
+// - region
+// - name
+// - pull-secret-path
+// Put bin data code from standard install-config and replace it with latest
+//    after reading from flags and generate the config file
+// Generate the manifest files from config.
+// Copy the cluster-network-operator-03.yaml to the manifests directory
+// Run the installer again. This should complete the setup
+
+}
