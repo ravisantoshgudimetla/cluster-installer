721371c64041232d090b77d244c3fab8e028a060
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2020-06-03 23:05:36 -0400

    Add delete subcommand

---
+++ cmd/delete.go
@@ -0,0 +1,77 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+
+	"github.com/ravisantoshgudimetla/cluster-installer/pkg/goinstaller"
+	"github.com/spf13/cobra"
+)
+
+var deleteInfo struct {
+	deleteInstaller bool
+	platform        string
+}
+
+func init() {
+	deleteCmd := newDeleteCmd()
+	ociCmd.AddCommand(deleteCmd)
+}
+
+func newDeleteCmd() *cobra.Command {
+	deleteCmd := &cobra.Command{
+		Use:              "delete",
+		Short:            "Deletes OpenShift cluster",
+		Long:             "deletes an OpenShift cluster",
+		TraverseChildren: true,
+		RunE: func(_ *cobra.Command, args []string) error {
+			return deleteInstaller(ociInfo.installerPath, deleteInfo.platform)
+		},
+	}
+	deleteCmd.PersistentFlags().BoolVar(&deleteInfo.deleteInstaller, "delete-installer", false,
+		"deletes the installer binary")
+	deleteCmd.PersistentFlags().StringVar(&deleteInfo.platform, "platform", "",
+		"deletes the specific directory after installation is complete")
+	return deleteCmd
+}
+
+func deleteInstaller(installerPath, platform string) error {
+	var errStdout, errStderr error
+	installDirectory := installerPath + "/openshift-install-linux/" + platform
+	cmd := exec.Command(installerPath+"/openshift-install-linux/"+"openshift-install", "destroy", "cluster", "--log-level=debug",
+		"--dir="+installDirectory)
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	stdout := goinstaller.NewCapturingPassThroughWriter(os.Stdout)
+	stderr := goinstaller.NewCapturingPassThroughWriter(os.Stderr)
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		_, errStdout = io.Copy(stdout, stdoutIn)
+		wg.Done()
+	}()
+
+	_, errStderr = io.Copy(stderr, stderrIn)
+	wg.Wait()
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatalf("failed to capture stdout or stderr\n")
+	}
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+	return nil
+}
