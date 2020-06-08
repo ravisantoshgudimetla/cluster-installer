58c838e75d1923034c24b3b13a6fd4fb3804759f
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2020-06-08 01:36:48 -0400

    Remove comments

--- main.go
+++ main.go
@@ -6,28 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	// var platform, region, name, publicKeyPath, pullSecretPath, installerPath string
-	// var skipDownload, version bool
-	// flag.StringVar(&platform, "platform", "aws", "specify the platform to use, defaults to aws")
-	// flag.StringVar(&region, "region", "us-east-2", "specify the region to use, defaults to us-east-2 for aws")
-	// flag.StringVar(&name, "name", "my-dev-cluster",
-	// 	"specify the name of cluster to be created, defaults to my-dev-cluster")
-	// flag.StringVar(&publicKeyPath, "key", "", "path to public key to be used, defaults to empty string be careful")
-	// flag.StringVar(&pullSecretPath, "pull-secret", "", "path to pull secret, be careful")
-	// flag.BoolVar(&skipDownload, "skip-download", false, "skips the download of the openshift install file")
-	// flag.StringVar(&installerPath, "installer-path", "/tmp", "installation directory")
-	// flag.BoolVar(&version, "version", false, "version")
-	// flag.Parse()
-	// if version {
-	// 	fmt.Printf("version: %s\n", cmd.GetVersion())
-	// 	os.Exit(0)
-	// }
-	// iOpts := goinstaller.NewInstallerOptions(platform, region, name, pullSecretPath, publicKeyPath, installerPath,
-	// 	skipDownload)
-	// if err := iOpts.Validate(); err != nil {
-	// 	log.Fatalf("error validating installer options: %v", err)
-	// }
-	// if err := iOpts.RunInstaller(); err != nil {
-	// 	log.Fatalf("error running installer: %v", err)
-	// }
 }
